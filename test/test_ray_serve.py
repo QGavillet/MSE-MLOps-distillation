563762f01f8b98f4b6242d0faadd9726e9058b24
Simon Braillard <simon.braillard@hotmail.com>
2024-12-11 15:28:24 +0100

    updating test

--- test/test_ray_serve.py
+++ test/test_ray_serve.py
@@ -6,7 +6,20 @@ with open("test/test_image.jpg", "rb") as f:
 
 # The deployment is assumed to be running at http://localhost:8000/
 # Make sure the deployment is running before sending the request.
-response = requests.post("http://localhost:8000/", data=image_bytes, headers={"Content-Type": "application/octet-stream"})
+response = requests.post("http://localhost:8000/teacher", data=image_bytes, headers={"Content-Type": "application/octet-stream"})
 
+# Print out the prediction result from the server
+print("Teacher model prediction:")
+print(response.text)
+
+# Replace 'path/to/your/test_image.jpg' with the actual path to an image file on your system.
+with open("test/test_image.jpg", "rb") as f:
+    image_bytes = f.read()
+
+# The deployment is assumed to be running at http://localhost:8000/
+# Make sure the deployment is running before sending the request.
+response = requests.post("http://localhost:8000/student", data=image_bytes, headers={"Content-Type": "application/octet-stream"})
+
+print("Student model prediction:")
 # Print out the prediction result from the server
 print(response.text)
